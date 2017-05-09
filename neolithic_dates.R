c4bcf90f6659fbdae9a04384f4719db294c469c1
nevrome <clemens@nevrome.de>
2017-05-09 23:19:44 +0200

    working with C14 dates, Gronenborns map and the new sf package

--- neolithic_dates.R
+++ neolithic_dates.R
@@ -1,31 +1,45 @@
-library(sp)
-library(proj4)
+# load libraries
+library(sf)
+library(magrittr)
+library(neolithicRtools)
+library(readr)
+library(purrr)
 
-neo_gronenborn <- rgdal::readOGR(
-  dsn = "/home/clemens/neomod/neomod_datapool/neolithic_expansion/gronenborn_map/neolithic.shp"
-)
+wgs <- 4326
 
-wgs <- "+init=epsg:4326"
+neo_gron <- sf::st_read(
+  dsn = "~/neomod/neomod_datapool/neolithic_expansion/gronenborn_map/neolithic.shp"
+  ) %>% sf::st_transform(wgs) 
 
-neo_gronenborn2 <- spTransform(neo_gronenborn, CRS(wgs))
+starts <- read_csv("~/neomod/neomod_datapool/neolithic_expansion/gronenborn_map/gronenborn.txt")
 
-CALPAL %<>% dplyr::filter(
+neo_gron <- neo_gron %>% dplyr::left_join(
+  starts, "id"
+  )
+
+# load data
+CALPAL_raw <- neolithicRtools::get_CALPAL()
+
+  # treat two null as NA in the coordinate columns
+CALPAL <- CALPAL_raw %>% transform(
+  LONGITUDE = ifelse(LONGITUDE == 0 & LATITUDE == 0, NA, LONGITUDE), 
+  LATITUDE = ifelse(LONGITUDE == 0 & LATITUDE == 0, NA, LATITUDE)
+  # remove NA coordinates
+  ) %>% dplyr::filter(
   !is.na(LONGITUDE) &
-  !is.na(LATITUDE)
-)
+    !is.na(LATITUDE)
+  # create sf object
+  ) %>% st_as_sf(
+  coords = c("LONGITUDE", "LATITUDE"),
+  crs = wgs
+  )
 
-CALPALspdf <- SpatialPointsDataFrame(
-  coords = data.frame(
-    as.numeric(CALPAL$LONGITUDE), 
-    as.numeric(CALPAL$LATITUDE)
-  ),
-  data = CALPAL,
-  proj4string = CRS(wgs)
-)
+hu <- st_within(CALPAL, neo_gron) %>% 
+  as.character() %>%
+  as.numeric() 
 
+CALPAL <- cbind(CALPAL, hu) %>%
+  dplyr::filter(!is.na(hu))
 
+plot(CALPAL["hu"])
 
-plot(neo_gronenborn2, axes = TRUE)
-plot(CALPALspdf, add = TRUE)
-
-plot(CALPALspdf, axes = TRUE)
