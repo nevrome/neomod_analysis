6357c6ea10eef7dc55c3f8a0ccf8b063ba9fd5b8
nevrome <clemens@nevrome.de>
2017-05-10 23:12:45 +0200

    improve interleaving of 14C dates and Gronenborns shapes

--- neolithic_dates.R
+++ neolithic_dates.R
@@ -4,6 +4,8 @@ library(magrittr)
 library(neolithicRtools)
 library(readr)
 library(purrr)
+library(dplyr)
+library(ggplot2)
 
 wgs <- 4326
 
@@ -14,7 +16,9 @@ neo_gron <- sf::st_read(
 starts <- read_csv("~/neomod/neomod_datapool/neolithic_expansion/gronenborn_map/gronenborn.txt")
 
 neo_gron <- neo_gron %>% dplyr::left_join(
-  starts, "id"
+    starts, "id"
+  ) %>% dplyr::mutate(
+    shape_index = 1:nrow(.)
   )
 
 # load data
@@ -34,12 +38,37 @@ CALPAL <- CALPAL_raw %>% transform(
   crs = wgs
   )
 
-hu <- st_within(CALPAL, neo_gron) %>% 
+# calculate, which 14C points are within which neogron shape
+shape_index <- st_within(CALPAL, neo_gron) %>% 
   as.character() %>%
   as.numeric() 
 
-CALPAL <- cbind(CALPAL, hu) %>%
-  dplyr::filter(!is.na(hu))
+  
+CALPAL2 <- CALPAL %>% 
+  # add column for shape indizes
+  cbind(shape_index) %>%
+  # remove all dates outside of the neogron shapes
+  dplyr::filter(!is.na(shape_index)) %>%
+  # merge neo_gron shape information with 14C dates
+  dplyr::left_join(
+    as.data.frame(neo_gron)[c("id", "start", "shape_index")], 
+    by = "shape_index"
+  ) %>%
+  # remove dates to far from the neogron-timeframe
+  filter(
+    !(CALAGE <= start - 500) &
+      !(CALAGE >= start + 500)
+  ) %>%
+  # calculate time distance
+  dplyr::mutate(
+    time_distance = start - CALAGE
+  )
 
-plot(CALPAL["hu"])
+
+plot(neo_gron["shape_index"])
+plot(CALPAL2["time_distance"], add = TRUE)
+
+ggplot() +
+  geom_sf(data = neo_gron) +
+  geom_sf(aes(color = time_distance), CALPAL2)
 
