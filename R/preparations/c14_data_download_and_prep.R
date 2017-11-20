f528205e9bab218371f1ac35ed91138e6b3d547c
nevrome <clemens@nevrome.de>
2017-11-20 17:59:51 +0100

    changed calibration in data separation to work not with the calage but with the real date density distribution

--- R/preparations/c14_data_download_and_prep.R
+++ R/preparations/c14_data_download_and_prep.R
@@ -1,15 +1,54 @@
 library(magrittr)
 library(ggplot2)
 
-bronze <- c14bazAAR::get_RADONB()
+bronze <- c14bazAAR::get_RADONB() %>%
+  # remove dates without age
+  dplyr::filter(!is.na(c14age) & !is.na(c14std)) %>%
+  # remove dates outside of calibration range
+  dplyr::filter(!(c14age < 71) & !(c14age > 46401))
 
+#bronze <- bronze[1:1000,]
 
-
+bronze <- bronze %>%
+  dplyr::mutate(
+    calage_density_distribution = Bchron::BchronCalibrate(
+      ages      = bronze$c14age,
+      ageSds    = bronze$c14std,
+      calCurves = rep("intcal13", nrow(bronze)),
+      eps       = 1e-06
+    ) %>% lapply(
+      function(x) {
+        tibble::tibble(age = x$ageGrid, dens_dist = x$densities) 
+      }
+    ) 
+  )
 
 save(bronze, file = "../neomod_datapool/bronze_age/bronze.RData")
+
+# plot(
+#   bronze$calage_density_distribution[[3]]$age, 
+#   bronze$calage_density_distribution[[3]]$dens_dist
+# )
+
 load("../neomod_datapool/bronze_age/bronze.RData")
 
-bronze2 <- bronze %>%
+bol <- 1950
+
+# time filter
+bronze0 <- bronze %>% 
+  dplyr::mutate(
+    in_time_of_interest = 
+      purrr::map(calage_density_distribution, function(x){
+        any(x$age <= (2500 + bol) & x$age >= (500 + bol))
+      }
+    )
+  ) %>%
+  dplyr::filter(
+    in_time_of_interest == TRUE
+  ) %>%
+  dplyr::select(-in_time_of_interest)
+
+bronze1 <- bronze0 %>%
   dplyr::select(
     -sourcedb, -c14age, -c14std, - c13val, -country, -shortref
   ) %>%
@@ -36,26 +75,19 @@ bronze2 <- bronze %>%
         "unknown"
       )
     )
-  ) %>% 
-  dplyr::mutate(
-    calage = calage - 1950
-  ) %>%
-  dplyr::filter(
-    calage >= 500 & calage <= 2500
-  ) %>%
-  dplyr::mutate(
-    age_class = cut(
-      calage, 
-      breaks = seq(500, 2500, 250), 
-      labels = paste0(seq(750, 2500, 250), "-", seq(500, 2250, 250), "calBC")
-    ) %>% as.factor %>% factor(levels = rev(levels(.)))
   ) %>%
   dplyr::filter(
     lat > 20, lon < 37
+  )
+
+bronze2 <- bronze1 %>%
+  tidyr::unnest(calage_density_distribution) %>%
+  dplyr::mutate(
+    age = age - bol
   ) %>%
-  # dplyr::filter(
-  #   !(burial_type == "unknown" & burial_construction == "unknown")
-  # ) %>%
+  dplyr::filter(
+    age <= 2500 & age >= 500
+  ) %>%
   dplyr::arrange(
     desc(burial_construction)
   )
