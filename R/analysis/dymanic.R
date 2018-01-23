ee7921d2e6939c0dca6b6a965e6a47b6156fc528
nevrome <clemens@nevrome.de>
2018-01-23 13:53:44 +0100

    more experiments with spline derivatives

--- R/analysis/dymanic.R
+++ R/analysis/dymanic.R
@@ -5,18 +5,25 @@ cremation <- proportion_per_region_df %>%
     idea != "mound" & idea != "flat" & idea != "inhumation"
   ) %>%
   dplyr::filter(
-    #region_name == "Northeast France and Benelux"
+    region_name == "Northeast France and Benelux"
     #region_name == "Northern Germany"
     #region_name == "Great Britain and Ireland"
-    region_name == "Jutland"
+    #region_name == "Jutland"
   )
 
+# po <- cremation %>%
+#   lm(timestep ~ proportion, data = .)
+# 
+# f_of_x <- splinefun(cremation$timestep, cremation$proportion, method = "fmm")
+# 
+# plot(cremation$timestep, cremation$proportion, "l")
+# plot(cremation$timestep, f_of_x(cremation$timestep, deriv = 0), "l", add = T, col = "red")
+# points(cremation$timestep, f_of_x(cremation$timestep, deriv = 1), "l", add = T)
 
-library(splines)
+schmooth <- smooth.spline(cremation$timestep, cremation$proportion, spar = 0.5)
 
-#kuu <- spline(cremation$timestep, cremation$proportion, method = "fmm")
+plot(cremation$timestep, cremation$proportion, "l")
+lines(cremation$timestep, predict(schmooth, cremation$timestep, deriv = 0)$y, "l", col = "red")
+plot(cremation$timestep, predict(schmooth, cremation$timestep, deriv = 1)$y, "l", col = "blue")
 
-f_of_x <- splinefun(cremation$timestep, cremation$proportion, method = "fmm")
 
-plot(cremation$timestep, f_of_x(cremation$timestep, deriv = 0), "l")
-points(cremation$timestep, f_of_x(cremation$timestep, deriv = 1), "l", add = T)
