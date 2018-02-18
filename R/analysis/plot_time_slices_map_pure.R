84913a4368de447644fc27da623c1bb8e3df0d14
nevrome <clemens@nevrome.de>
2018-02-18 18:19:58 +0100

    improved movie map plot

--- R/analysis/plot_time_slices_map_pure.R
+++ R/analysis/plot_time_slices_map_pure.R
@@ -1,8 +1,8 @@
-load("../neomod_datapool/bronze_age/space_and_network/land_outline_sf.RData")
+#load("../neomod_datapool/bronze_age/space_and_network/land_outline_sf.RData")
 load("../neomod_datapool/bronze_age/bronze2.RData")
 
-regions <- sf::st_read(
-  "manually_changed_data/regionen2017g.shp"
+land_outline <- sf::st_read(
+  "../neomod_datapool/geodata/land_shapes/ne_50m_land.shp"
 ) %>% 
   sf::st_transform(4326)
 
@@ -11,6 +11,20 @@ countries <- sf::st_read(
 ) %>% 
   sf::st_transform(4326)
 
+rivers <- sf::st_read(
+  "../neomod_datapool/geodata/rivers_lakes_shapes/ne_50m_rivers_lake_centerlines_scale_rank.shp"
+) %>% 
+  sf::st_transform(4326)
+
+lakes <- sf::st_read(
+  "../neomod_datapool/geodata/rivers_lakes_shapes/ne_50m_lakes.shp"
+) %>% 
+  sf::st_transform(4326)
+
+regions <- sf::st_read(
+  "manually_changed_data/regionen2017g.shp"
+) %>% 
+  sf::st_transform(4326)
 
 bronze2_slices <- bronze2 %>%
   dplyr::filter(
@@ -21,16 +35,24 @@ bronze2_slices <- bronze2 %>%
   )
 
 library(ggplot2)
+library(sf)
 
-limit <- sf::st_bbox(land_outline)
-xlimit <- limit[c(1, 3)]  
-ylimit <- limit[c(2, 4)]  
+xlimit <- c(-1600000, 1200000)
+ylimit <- c(800000, 3600000)
 
 hu <- ggplot() +
   geom_sf(
     data = countries,
     fill = NA, colour = "black", size = 0.2
   ) +
+  # geom_sf(
+  #   data = rivers,
+  #   fill = NA, colour = "black", size = 0.2
+  # ) +
+  # geom_sf(
+  #   data = lakes,
+  #   fill = NA, colour = "black", size = 0.2
+  # ) +
   # geom_sf(
   #   data = land_outline,
   #   fill = NA, colour = "black", size = 0.2
@@ -111,26 +133,33 @@ hu %>%
 
 #### movie ####
 
-for(i in 2500:500) {
+for(i in seq(2500, 500, -10)) {
   bronze2_fine_slices <- bronze2 %>%
     dplyr::filter(
       age == i
+    ) %>% st_as_sf(
+      coords = c("lon", "lat"),
+      crs = 4326
     )
 
   hu <- ggplot() +
-    geom_polygon(
-      data = area,
-      aes_string(
-        x = "long", y = "lat",
-        group = "group"
-      ),
-      fill = NA, colour = "black",
-      size = 0.1
+    geom_sf(
+      data = land_outline,
+      fill = NA, colour = "black", size = 0.4
     ) +
-    geom_point(
+    geom_sf(
+      data = rivers,
+      fill = NA, colour = "black", size = 0.2
+    ) +
+    geom_sf(
+      data = lakes,
+      fill = NA, colour = "black", size = 0.2
+    ) +
+    #geom_point(
+    geom_sf(
       data = bronze2_fine_slices, 
       aes(
-        x = lon, y = lat, 
+        #x = lon, y = lat,
         color = burial_type, 
         shape = burial_construction,
         size = burial_construction,
@@ -138,10 +167,10 @@ for(i in 2500:500) {
       )
     ) +
     theme_bw() +
-    coord_map(
-      "ortho", orientation = c(48, 13, 0),
-      xlim = c(-10, 30), ylim = c(35, 65)
-    ) +
+    coord_sf(
+      xlim = xlimit, ylim = ylimit,
+      crs = st_crs(102013)
+    ) + 
     scale_shape_manual(
       values = c(
         "flat" = "\u268A",
@@ -151,19 +180,20 @@ for(i in 2500:500) {
     ) +
     scale_size_manual(
       values = c(
-        "flat" = 3,
-        "mound" = 3,
-        "unknown" = 2
+        "flat" = 10,
+        "mound" = 10,
+        "unknown" = 7.5
       )
     ) +
     scale_color_manual(
       values = c(
-        "cremation" = "red",
-        "inhumation" = "darkgreen",
+        "cremation" = "#D55E00",
+        "inhumation" = "#0072B2",
+        "mound" = "#CC79A7",
+        "flat" = "#009E73",
         "unknown" = "darkgrey"
       )
     ) +
-    scale_alpha(guide = FALSE) +
     theme(
       axis.title.x = element_blank(),
       axis.text.x = element_blank(),
@@ -171,8 +201,15 @@ for(i in 2500:500) {
       axis.title.y = element_blank(),
       axis.text.y = element_blank(),
       axis.ticks.y = element_blank(),
-      legend.position = c(1, 0), legend.justification = c(1, 0)
-      #legend.position = "bottom"
+      plot.title = element_text(size = 30, face = "bold"),
+      legend.position = "right",
+      legend.title = element_text(size = 20, face = "bold"),
+      legend.text = element_text(size = 15)
+    ) +
+    guides(
+      title = "Grabtyp", color = guide_legend(nrow = 3, byrow = TRUE),
+      shape = guide_legend(nrow = 3, byrow = TRUE),
+      alpha = FALSE
     ) +
     ggtitle(paste0(i, "calBC"))
   
@@ -183,7 +220,7 @@ for(i in 2500:500) {
       device = "jpeg",
       scale = 1,
       dpi = 300,
-      width = 200, height = 150, units = "mm",
+      width = 400, height = 300, units = "mm",
       limitsize = F
     )
 
