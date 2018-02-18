4d930b586aed286b34acef1ece08444bf0c17fed
nevrome <clemens@nevrome.de>
2018-02-18 16:22:08 +0100

    some counts to get info about the data in radonb

--- R/preparations/100_prepare_c14_data.R
+++ R/preparations/100_prepare_c14_data.R
@@ -100,6 +100,36 @@ bronze1 <- bronze0 %>%
 save(bronze1, file = "../neomod_datapool/bronze_age/bronze1.RData")
 load("../neomod_datapool/bronze_age/bronze1.RData")
 
+# get some counts
+bronze1 %>%
+  dplyr::group_by(
+    burial_type
+  ) %>%
+  dplyr::summarise(
+    n()
+  )
+
+bronze1 %>%
+  dplyr::group_by(
+    burial_construction
+  ) %>%
+  dplyr::summarise(
+    n()
+  )
+
+bronze1 %>%
+  dplyr::filter(
+    burial_type != "unknown",
+    burial_construction != "unknown"
+  ) %>%
+  dplyr::group_by(
+    burial_type, burial_construction
+  ) %>%
+  dplyr::summarise(
+    n = n()
+  ) %$%
+  sum(n)
+
 # open up to diachron perspective
 bronze2 <- bronze1 %>%
   tidyr::unnest(calage_density_distribution) %>%
