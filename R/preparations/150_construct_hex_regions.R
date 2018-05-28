e7aee7e08a71db30a03aafd69bb8c9835af114b3
nevrome <clemens@nevrome.de>
2018-05-28 23:26:06 +0200

    added region names

--- R/preparations/150_construct_hex_regions.R
+++ R/preparations/150_construct_hex_regions.R
@@ -12,7 +12,7 @@ area <- sf::st_intersection(sf::st_buffer(land_outline, 0), research_area)
 
 region_circles <- tibble::tibble(
   geometry = sf::st_make_grid(area, 400000, what = "centers"),
-  region_id = 1:length(geometry)
+  ID = 1:length(geometry)
   ) %>% sf::st_as_sf()
 region_circles <- sf::st_intersection(region_circles, research_area)
 region_circles %<>% sf::st_buffer(dist = 300000)
@@ -35,21 +35,52 @@ ggplot() +
   geom_sf(data = schnu)
 
 regions_with_enough_graves <- schnu %>% 
-  dplyr::group_by(region_id) %>%
+  dplyr::group_by(ID) %>%
   dplyr::summarise(
     n = n()
   ) %>%
   dplyr::filter(
     n >= 100
   ) %$%
-  region_id
+  ID
 
-selected_region_circles <- region_circles %>%
-  dplyr::filter(region_id %in% regions_with_enough_graves)
+regions <- region_circles %>%
+  dplyr::filter(ID %in% regions_with_enough_graves)
 
 ggplot() +
   geom_sf(data = area) +
-  geom_sf(data = selected_region_circles, fill = NA) +
+  geom_sf(data = regions, fill = NA) +
   geom_sf(data = schnu)
 
-save(selected_region_circles, file = "../neomod_datapool/bronze_age/selected_region_circles.RData")
+regions %>%
+  dplyr::mutate(
+    x = purrr::map_dbl(geometry, ~sf::st_centroid(.x)[[1]]),
+    y = purrr::map_dbl(geometry, ~sf::st_centroid(.x)[[2]])
+  ) %>%
+  ggplot() +
+    geom_sf(data = area) +
+    geom_sf(fill = NA) +
+    geom_text(aes(x = x, y = y, label = ID))
+
+regions$ID <- 1:nrow(regions)
+regions$NAME <- c(
+  "Northeast France", 
+  "Southern Germany", 
+  "Austria and Czech Republic", 
+  "England", "Benelux", 
+  "Northern Germany",
+  "Poland",
+  "Southern Skandinavia"
+  )
+
+regions %>%
+  dplyr::mutate(
+    x = purrr::map_dbl(geometry, ~sf::st_centroid(.x)[[1]]),
+    y = purrr::map_dbl(geometry, ~sf::st_centroid(.x)[[2]])
+  ) %>%
+  ggplot() +
+  geom_sf(data = area) +
+  geom_sf(fill = NA) +
+  geom_text(aes(x = x, y = y, label = NAME))
+
+save(regions, file = "../neomod_datapool/bronze_age/regions.RData")
