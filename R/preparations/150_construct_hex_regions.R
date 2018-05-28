7b26bd04c05cef410e7906208a86da23d7aac8d7
nevrome <clemens@nevrome.de>
2018-05-28 22:13:35 +0200

    got rid of hexes in favor of overlapping circles

--- R/preparations/150_construct_hex_regions.R
+++ R/preparations/150_construct_hex_regions.R
@@ -10,40 +10,11 @@ land_outline <- sf::st_read(
 
 area <- sf::st_intersection(sf::st_buffer(land_outline, 0), research_area)
 
-library(sp)
-library(raster)
-library(rgeos)
-area_sp <- as(area, 'Spatial')
-make_grid <- function(x, cell_diameter, cell_area, clip = FALSE) {
-  if (missing(cell_diameter)) {
-    if (missing(cell_area)) {
-      stop("Must provide cell_diameter or cell_area")
-    } else {
-      cell_diameter <- sqrt(2 * cell_area / sqrt(3))
-    }
-  }
-  ext <- as(extent(x) + cell_diameter, "SpatialPolygons")
-  projection(ext) <- projection(x)
-  # generate array of hexagon centers
-  g <- spsample(ext, type = "hexagonal", cellsize = cell_diameter, 
-                offset = c(0.5, 0.5))
-  # convert center points to hexagons
-  g <- HexPoints2SpatialPolygons(g, dx = cell_diameter)
-  # clip to boundary of study area
-  if (clip) {
-    g <- gIntersection(g, x, byid = TRUE)
-  } else {
-    g <- g[x, ]
-  }
-  # clean up feature IDs
-  row.names(g) <- as.character(1:length(g))
-  return(g)
-}
+hu <- tibble::tibble(geometry = sf::st_make_grid(area, 400000, what = "centers")) %>% sf::st_as_sf()
 
-set.seed(19)
-hex_grid <- make_grid(area_sp, cell_diameter = 520000, clip = FALSE)
+hu <- sf::st_intersection(hu, research_area)
 
-hex_grid %<>% sf::st_as_sf() 
+hu %<>% sf::st_buffer(dist = 300000)
 
 load("../neomod_datapool/bronze_age/bronze1.RData")
 bronze1 %<>% sf::st_as_sf(coords = c("lon", "lat"))
@@ -52,7 +23,10 @@ sf::st_crs(bronze1) <- 4326
 library(ggplot2)
 ggplot() +
   geom_sf(data = area) +
-  geom_sf(data = hex_grid, fill = NA) +
+  geom_sf(data = hu, fill = NA) +
   geom_sf(data = bronze1)
+
+
+
 
 
