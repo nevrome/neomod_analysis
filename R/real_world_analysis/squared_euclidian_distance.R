d46843659dd439e9f9f89f479c0e40fdeb5cc639
nevrome <clemens@nevrome.de>
2018-06-14 15:52:23 +0200

    made sed plot more nice

--- R/real_world_analysis/squared_euclidian_distance.R
+++ R/real_world_analysis/squared_euclidian_distance.R
@@ -1,7 +1,8 @@
 load("../neomod_datapool/bronze_age/development_proportions_burial_type.RData")
 load("../neomod_datapool/bronze_age/development_proportions_burial_construction.RData")
 
-proportion_development_burial_type %>% tibble::as.tibble()
+prop <- proportion_development_burial_type
+# prop <- proportion_development_burial_construction
 
 # proportion_development_burial_type %>%
 #   tidyr::complete(
@@ -9,7 +10,7 @@ proportion_development_burial_type %>% tibble::as.tibble()
 #     fill = list(proportion = as.integer(0))
 #   )
 
-by_regions <-  base::split(proportion_development_burial_type, proportion_development_burial_type$region_name)
+by_regions <-  base::split(prop, prop$region_name)
 
 by_regions_and_time <- lapply(
   by_regions,
@@ -18,8 +19,8 @@ by_regions_and_time <- lapply(
   }
 )
 
-regions <- proportion_development_burial_type$region_name %>% unique()
-timesteps <- proportion_development_burial_type$timestep %>% unique()
+regions <- prop$region_name %>% unique()
+timesteps <- prop$timestep %>% unique()
 regions_grid <- expand.grid(regionA = regions, regionB = regions, time = timesteps, stringsAsFactors = FALSE)
 
 
@@ -35,12 +36,60 @@ for (p1 in 1:nrow(regions_grid)) {
   regions_grid$sed[p1] <- sed(p_region_A, p_region_B)
 }
 
+
+regions_factorA <- as.factor(regions_grid$regionA)
+regions_grid$regionA <- factor(regions_factorA, levels = c(
+  "Austria and Czech Republic",
+  "Poland",
+  "Southern Germany",
+  "Northeast France",
+  "Northern Germany",
+  "Southern Skandinavia",
+  "Benelux",
+  "England"
+))
+
+regions_factorB <- as.factor(regions_grid$regionB)
+regions_grid$regionB <- factor(regions_factorB, levels = c(
+  "Austria and Czech Republic",
+  "Poland",
+  "Southern Germany",
+  "Northeast France",
+  "Northern Germany",
+  "Southern Skandinavia",
+  "Benelux",
+  "England"
+))
+
 library(ggplot2)
-regions_grid %>%
+hu <- regions_grid %>%
   ggplot() +
-  geom_line(aes(time, sed)) +
+  geom_line(
+    aes(time, sed),
+    alpha = 0.3
+  ) +
+  geom_smooth(
+    aes(time, sed, color = regionB),
+    method = "loess",
+    span = 0.3
+  ) +
   facet_grid(
-    regionA ~ regionB
-  )
+    regionA ~ regionB,
+    switch = "y"
+  ) +
+  scale_x_reverse() +
+  theme_bw() +
+  scale_color_discrete(guide = FALSE)
   
+hu %>%
+  ggsave(
+    "/home/clemens/neomod/neomod_datapool/bronze_age/regions_squared_euclidian_distance_burial_type.jpeg",
+    #"/home/clemens/neomod/neomod_datapool/bronze_age/development_proportions_regions_burial_construction.jpeg",
+    plot = .,
+    device = "jpeg",
+    scale = 1,
+    dpi = 300,
+    width = 300, height = 300, units = "mm",
+    limitsize = F
+  )
 
