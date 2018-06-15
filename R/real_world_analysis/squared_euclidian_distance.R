2c404c8af02db960851461c44fbf076277b172e1
nevrome <clemens@nevrome.de>
2018-06-15 14:37:59 +0200

    simple plot of the mean sed correlation matrix

--- R/real_world_analysis/squared_euclidian_distance.R
+++ R/real_world_analysis/squared_euclidian_distance.R
@@ -109,6 +109,58 @@ hu %>%
     limitsize = F
   )
 
+#### mean sed ####
+
+regions_grid_mean <- regions_grid %>%
+  dplyr::group_by(
+    regionA, regionB
+  ) %>%
+  dplyr::summarise(
+    mean_sed = mean(sed, na.rm = T)
+  )
+
+regions_grid_mean$regionB <- forcats::fct_rev(regions_grid_mean$regionB)
+
+kur <- regions_grid_mean %>%
+  ggplot() +
+  geom_raster(
+    aes(
+      x = regionA,
+      y = regionB,
+      fill = mean_sed
+    )
+  ) +
+  geom_text(
+    aes(
+      x = regionA,
+      y = regionB,
+      label = round(mean_sed, 2)
+    )
+  ) +
+  scale_x_discrete(position = "top") +
+  scale_fill_continuous(
+    guide = FALSE,
+    high = "#6d6d6d", low = "#FFFFFF"
+  ) +
+  theme_bw() +
+  theme(
+    axis.text.x = element_text(size = 10, angle = 45, vjust = 1, hjust = 0),
+    axis.text.y = element_text(size = 10, angle = 45, vjust = 0, hjust = 1),
+    axis.title = element_blank()
+  )
+
+kur %>%
+  ggsave(
+    "/home/clemens/neomod/neomod_datapool/bronze_age/regions_regions_mean_squared_euclidian_distance_burial_type.jpeg",
+    #"/home/clemens/neomod/neomod_datapool/bronze_age/development_proportions_regions_burial_construction.jpeg",
+    plot = .,
+    device = "jpeg",
+    scale = 1,
+    dpi = 300,
+    width = 200, height = 200, units = "mm",
+    limitsize = F
+  )
+
 #### second sed plot ####
 
 schnu <- regions_grid %>%
