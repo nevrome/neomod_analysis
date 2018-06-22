21f866cb640f743ae21abef6e2a6fcb347d10e43
nevrome <clemens@nevrome.de>
2018-06-22 15:38:04 +0200

    spearman vs. pearson

--- R/real_world_analysis/mantel_test.R
+++ R/real_world_analysis/mantel_test.R
@@ -2,11 +2,11 @@ load("/home/clemens/neomod/neomod_datapool/bronze_age/distance_matrix_spatial.RD
 load("/home/clemens/neomod/neomod_datapool/bronze_age/distance_matrizes_sed_burial_type.RData")
 
 library(vegan)
-mantel(distance_matrix_spatial, distance_matrizes_sed_burial_type[[1]], method = "pearson", permutations=999)
+mantel(distance_matrix_spatial, distance_matrizes_sed_burial_type[[1]], method = "spear", permutations=999)
 
 lapply(
   distance_matrizes_sed_burial_type, function(x, y) {
-    mantel(x, y, method = "pearson", permutations=999)
+    mantel(x, y, method = "spear", permutations=999)
   },
   y = distance_matrix_spatial
 )
