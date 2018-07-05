3369b8649cdfd816e0c7086cd85b6ae0c2084423
nevrome <clemens@nevrome.de>
2018-07-05 12:56:55 +0200

    got rid of old region pictograms and improved the new ones

--- R/real_world_data_preparation/250_region_pictograms.R
+++ R/real_world_data_preparation/250_region_pictograms.R
@@ -2,36 +2,6 @@ load("../neomod_datapool/bronze_age/regions.RData")
 load("../neomod_datapool/bronze_age/spatial_data/extended_area.RData")
 extended_area <- extended_area$geometry
 
-#### grey ####
-
-path = "../neomod_datapool/bronze_age/region_pictograms/"
-
-for (i in 1:nrow(regions)) {
-  
-  one_region <- regions[i, ]
-  one_region_geom <- one_region$geometry
-  
-  one_region_name <- one_region$NAME %>% gsub(" ", "_", ., fixed = TRUE)
-  
-  png(
-    filename = paste0(path, one_region_name, ".png"),
-    width = 87*4, height = 100*4, units = "px", res = 300
-  )
-  par(mar = c(0,0,0,0),
-      pin = c(4,2),
-      pty = "m",
-      xaxs = "i",
-      xaxt = 'n',
-      xpd = FALSE,
-      yaxs = "i",
-      yaxt = 'n',
-      bg = NA)
-  plot(extended_area, border = NA , col = "grey85", lwd = 2)
-  plot(one_region_geom, border = "red", col = NA, lwd = 3, add = TRUE)
-  dev.off()
-
-}
-
 #### colour #### 
 
 regions_ordered <- c(
@@ -83,7 +53,7 @@ for (i in 1:nrow(regions)) {
       yaxt = 'n',
       bg = NA)
   plot(extended_area, border = NA , col = "grey85", lwd = 2)
-  plot(one_region_buffer_geom, border = NA, col = alpha(colour_vector[i], 0.6), add = TRUE, alpha = 0.5)
+  plot(one_region_buffer_geom, border = NA, col = alpha(colour_vector[i], 0.4), add = TRUE)
   plot(one_region_geom, border = NA, col = colour_vector[i], add = TRUE)
   dev.off()
   
