87f10f9fe40bf4054e8ded6599c5e36f0347c80f
nevrome <clemens@nevrome.de>
2018-07-06 17:24:03 +0200

    outsourcing of main variable mantel test

--- R/real_world_analysis/burial_construction_mantel_test.R
+++ R/real_world_analysis/burial_construction_mantel_test.R
@@ -24,31 +24,3 @@ save(
 )
 
 
-####
-
-load("/home/clemens/neomod/neomod_datapool/bronze_age/distance_matrizes_sed_burial_type.RData")
-dms_burial_type <- distance_matrizes_sed
-
-load("/home/clemens/neomod/neomod_datapool/bronze_age/distance_matrizes_sed_burial_construction.RData")
-dms_burial_construction <- distance_matrizes_sed
-
-mantel_test_results <- lapply(
-  1:length(dms_burial_type), function(i, x, y, z) {
-    mantel_result <- vegan::mantel(x[[i]], y[[i]], method = "pearson", permutations=999)
-    data.frame(
-      time = z[[i]],
-      statistic = mantel_result$statistic, 
-      signif = mantel_result$signif
-    )
-  },
-  x = dms_burial_type,
-  y = dms_burial_construction,
-  z = names(dms_burial_type)
-) %>%
-  do.call(rbind, .)
-
-save(
-  mantel_test_results, 
-  file = "../neomod_datapool/bronze_age/mantel_sed_spatial_burial_type_burial_construction.RData"
-)
-
