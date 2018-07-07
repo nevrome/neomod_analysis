2b201f34c19a550472fc9c6f2634d4a5cb0994b8
nevrome <clemens@nevrome.de>
2018-07-07 17:10:02 +0200

    sed matrix calculation for many simulation results

---
+++ R/simulation_analysis/sed/multiple_simulation_200_sed_create_matritzes.R
@@ -0,0 +1,40 @@
+load("../neomod_datapool/simulation_data/squared_euclidian_distance_over_time_sim_multiple.RData")
+
+modelid_time_regions_grid <- regions_grid %>% dplyr::mutate(
+  time = base::cut(
+    time, 
+    seq(-2200, -800, 200), labels = paste(seq(-2200, -1000, 200), seq(-2000, -800, 200), sep = " - "),
+    include.lowest = TRUE, 
+    right = FALSE)
+) %>%
+  dplyr::group_by(
+    model_id, time, regionA, regionB
+  ) %>%
+  dplyr::summarise(
+    mean_sed = mean(sed, na.rm = TRUE)
+  ) %>%
+  dplyr::ungroup() %>%
+  # that's dangerous...
+  dplyr::mutate(
+    mean_sed = tidyr::replace_na(mean_sed, 0)
+  )
+
+distance_matrizes_sed <- lapply(
+  base::split(modelid_time_regions_grid, modelid_time_regions_grid$model_id), function(time_regions_grid) {
+    lapply(
+      base::split(time_regions_grid, time_regions_grid$time), function(x){
+        x %>%
+          dplyr::select(
+            -model_id, -time
+          ) %>%
+          tidyr::spread(regionA, mean_sed) %>%
+          dplyr::select(
+            -regionB
+          ) %>%
+          as.matrix()
+      }
+    )
+  }
+)
+
+save(distance_matrizes_sed, file = "../neomod_datapool/simulation_data/distance_matrizes_sed_simulation.RData")
