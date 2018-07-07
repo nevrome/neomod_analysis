f3dc2502c00367a15e3f4430fe03d5dc6b7d3d7b
nevrome <clemens@nevrome.de>
2018-07-07 15:10:55 +0200

    ... again

---
+++ R/real_world_analysis/sed/burial_type_110_sed_region_matrix.R
@@ -0,0 +1,57 @@
+load("../neomod_datapool/R_data/squared_euclidian_distance_over_time_burial_type.RData")
+
+library(ggplot2)
+hu <- regions_grid %>%
+  ggplot() +
+  geom_line(
+    aes(time, sed),
+    alpha = 0.3,
+    size = 0.5
+  ) +
+  geom_smooth(
+    aes(time, sed, color = regionB),
+    method = "loess",
+    span = 0.3,
+    size = 1.5
+  ) +
+  facet_grid(
+    regionA ~ regionB,
+    switch = "y"
+  ) +
+  scale_x_continuous(
+    breaks = c(-2000, -1500, -1000), 
+    limits = c(-2000, -800)
+  ) +
+  theme_bw() +
+  scale_color_manual(
+    guide = FALSE,
+    values = c(
+      "Austria and Czechia" = "#999999", 
+      "Poland" = "#ffe500", 
+      "Southern Germany" = "#56B4E9", 
+      "Northeast France" = "#009E73", 
+      "Northern Germany" = "#000000", 
+      "Southern Skandinavia" = "#0072B2", 
+      "Benelux" = "#D55E00", 
+      "England" = "#CC79A7"
+    )
+  ) +
+  theme(
+    axis.text = element_text(size = 10),
+    axis.text.x = element_text(angle = 45, hjust = 1),
+    axis.title = element_text(size = 15),
+    strip.text = element_text(size = 9)
+  ) +
+  ylab("Sqared Euclidian Distance") +
+  xlab("Time")
+
+hu %>%
+  ggsave(
+    "/home/clemens/neomod/neomod_datapool/plots/sed/regions_regions_squared_euclidian_distance_burial_type.jpeg",
+    plot = .,
+    device = "jpeg",
+    scale = 1,
+    dpi = 300,
+    width = 300, height = 300, units = "mm",
+    limitsize = F
+  )
---
+++ R/real_world_analysis/sed/burial_type_120_sed_region_matrix_mean.R
@@ -0,0 +1,52 @@
+load("../neomod_datapool/R_data/squared_euclidian_distance_over_time_burial_type.RData")
+
+regions_grid_mean <- regions_grid %>%
+  dplyr::group_by(
+    regionA, regionB
+  ) %>%
+  dplyr::summarise(
+    mean_sed = mean(sed, na.rm = T)
+  )
+
+regions_grid_mean$regionB <- forcats::fct_rev(regions_grid_mean$regionB)
+
+save(regions_grid_mean, file = "../neomod_datapool/bronze_age/regions_mean_sed")
+
+kur <- regions_grid_mean %>%
+  ggplot() +
+  geom_raster(
+    aes(
+      x = regionA,
+      y = regionB,
+      fill = mean_sed
+    )
+  ) +
+  geom_text(
+    aes(
+      x = regionA,
+      y = regionB,
+      label = round(mean_sed, 2)
+    )
+  ) +
+  scale_x_discrete(position = "top") +
+  scale_fill_continuous(
+    guide = FALSE,
+    high = "#6d6d6d", low = "#FFFFFF"
+  ) +
+  theme_bw() +
+  theme(
+    axis.text.x = element_text(size = 10, angle = 45, vjust = 1, hjust = 0),
+    axis.text.y = element_text(size = 10, angle = 45, vjust = 0, hjust = 1),
+    axis.title = element_blank()
+  )
+
+kur %>%
+  ggsave(
+    "/home/clemens/neomod/neomod_datapool/plots/sed/regions_regions_mean_squared_euclidian_distance_burial_type.jpeg",
+    plot = .,
+    device = "jpeg",
+    scale = 1,
+    dpi = 300,
+    width = 200, height = 200, units = "mm",
+    limitsize = F
+  )
