31741f7b5104c45c64b6f3d4c0ae8529c67738aa
nevrome <clemens@nevrome.de>
2018-07-07 15:02:56 +0200

    fixing references and outsourcing

---
+++ R/real_world_analysis/sed/burial_construction_130_sed_regions.R
@@ -0,0 +1,76 @@
+load("../neomod_datapool/R_data/development_proportions_burial_construction.RData")
+
+schnu <- regions_grid %>%
+  ggplot() +
+  geom_smooth(
+    aes(time, sed, color = regionB),
+    method = "loess",
+    span = 0.3
+  ) +
+  facet_wrap(
+    ~regionA, 
+    nrow = 8
+  ) +
+  scale_x_continuous(
+    breaks = c(-2200, -2000, -1500, -1000, -800), 
+    limits = c(-2500, -800)
+  ) +
+  theme_bw() +
+  scale_color_manual(
+    guide = FALSE,
+    values = c(
+      "Austria and Czechia" = "#999999", 
+      "Poland" = "#ffe500", 
+      "Southern Germany" = "#56B4E9", 
+      "Northeast France" = "#009E73", 
+      "Northern Germany" = "#000000", 
+      "Southern Skandinavia" = "#0072B2", 
+      "Benelux" = "#D55E00", 
+      "England" = "#CC79A7"
+    )
+  ) +
+  theme(
+    legend.position = "bottom",
+    panel.grid.major.x = element_line(colour = "black", size = 0.3),
+    axis.text = element_text(size = 15),
+    axis.title = element_text(size = 15),
+    strip.text.x = element_text(size = 13),
+    legend.title = element_text(size = 15),
+    legend.text = element_text(size = 15)
+  ) +
+  ylab("Sqared Euclidian Distance") +
+  xlab("Time")
+
+
+region_file_list <- unique(regions_grid$regionA) %>% gsub(" ", "_", ., fixed = TRUE)
+
+gl <- lapply(region_file_list, function(x) {
+  img <- png::readPNG(paste0("../neomod_datapool/plots/region_pictograms_colour/", x, ".png"))
+  g <- grid::rasterGrob(
+    img, interpolate = TRUE,
+    width = 0.14, height = 1.2
+  )
+})
+dummy <- tibble::tibble(regionA = unique(regions_grid$regionA), grob = gl )
+
+source("R/helper_functions/geom_grob.R")
+
+schnu <- schnu +
+  geom_custom(
+    data = dummy, 
+    aes(grob = grob), 
+    inherit.aes = FALSE,
+    x = 0.1, y = 0.5
+  )
+
+schnu %>%
+  ggsave(
+    "/home/clemens/neomod/neomod_datapool/plots/sed/regions_squared_euclidian_distance_burial_construction.jpeg",
+    plot = .,
+    device = "jpeg",
+    scale = 1,
+    dpi = 300,
+    width = 210, height = 297, units = "mm",
+    limitsize = F
+  )
+
