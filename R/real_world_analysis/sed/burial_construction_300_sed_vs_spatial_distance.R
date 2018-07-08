30c05a10b9cc5c6f9c61eac587829df0182a0e89
nevrome <clemens@nevrome.de>
2018-07-08 14:40:41 +0200

    outsourcing data preparation and adjusting plot

--- R/real_world_analysis/sed/burial_construction_300_sed_vs_spatial_distance.R
+++ R/real_world_analysis/sed/burial_construction_300_sed_vs_spatial_distance.R
@@ -1,82 +1,8 @@
-load("../neomod_datapool/bronze_age/squared_euclidian_distance_over_time_burial_construction.RData")
-load("../neomod_datapool/bronze_age/distance_matrix_spatial_long.RData")
-load("../neomod_datapool/bronze_age/mantel_sed_spatial_burial_construction.RData")
-
-test <- regions_grid %>%
-  dplyr::mutate(
-    regionA = as.character(regionA),
-    regionB = as.character(regionB)
-  ) 
-
-distance_matrix_spatial_long %<>%
-  dplyr::mutate(
-    regionA = as.character(regionA),
-    regionB = as.character(regionB)
-  ) 
-
-test <- lapply(
-  split(test, f = test$time),
-  function(x) {
-    mn <- pmin(x$regionA, x$regionB)
-    mx <- pmax(x$regionA, x$regionB)
-    int <- as.numeric(interaction(mn, mx))
-    x <- x[match(unique(int), int),]
-    return(x)
-  }
-) %>%
-  do.call(rbind, .)
-
-hu <- test %>% dplyr::left_join(
-    distance_matrix_spatial_long, by = c("regionA", "regionB")
-  ) %>% 
-  dplyr::filter(
-    distance != 0
-  ) %>%
-  dplyr::mutate(
-    relation = paste(regionA, "+", regionB),
-    time = base::cut(
-      time, 
-      seq(-2200, -800, 200), labels = paste(seq(-2200, -1000, 200), seq(-2000, -800, 200), sep = " - "),
-      include.lowest = TRUE, 
-      right = FALSE)
-  ) %>%
-  dplyr::group_by(
-    time, regionA, regionB, distance
-  ) %>%
-  dplyr::summarise(
-    mean_sed = mean(sed, na.rm = TRUE)
-  ) %>%
-  dplyr::ungroup() %>%
-  dplyr::filter(
-    !is.na(mean_sed)
-  )
-
-regions_factorA <- as.factor(hu$regionA)
-hu$regionA <- factor(regions_factorA, levels = c(
-  "Austria and Czechia",
-  "Poland",
-  "Southern Germany",
-  "Northeast France",
-  "Northern Germany",
-  "Southern Skandinavia",
-  "Benelux",
-  "England"
-))
-
-regions_factorB <- as.factor(hu$regionB)
-hu$regionB <- factor(regions_factorB, levels = c(
-  "Austria and Czechia",
-  "Poland",
-  "Southern Germany",
-  "Northeast France",
-  "Northern Germany",
-  "Southern Skandinavia",
-  "Benelux",
-  "England"
-))
+load("../neomod_datapool/R_data/mantel_sed_spatial_burial_construction.RData")
+load("../neomod_datapool/R_data/squared_euclidian_distance_over_timeblocks_burial_construction.RData")
 
 library(ggplot2)
-plu <- ggplot(hu) +
+plu <- ggplot(sed_spatial_distance) +
   geom_boxplot(
     aes(x = distance, y = mean_sed, group = distance),
     width = 0.3
@@ -100,7 +26,7 @@ plu <- ggplot(hu) +
     x = 2.7, y = 2.2,
     size = 6
   ) +
-  facet_wrap(~time) +
+  facet_wrap(~time, nrow = 2) +
   theme_bw() +
   theme(
     plot.title = element_text(size = 30, face = "bold"),
@@ -151,12 +77,12 @@ plu <- ggplot(hu) +
 
 plu %>%
   ggsave(
-		"/home/clemens/neomod/neomod_datapool/bronze_age/squared_euclidian_distance_vs_spatial_distance_burial_construction.jpeg",
+		"../neomod_datapool/plots/sed/squared_euclidian_distance_vs_spatial_distance_burial_construction.jpeg",
     plot = .,
     device = "jpeg",
     scale = 1,
     dpi = 300,
-    width = 350, height = 360, units = "mm",
+    width = 550, height = 280, units = "mm",
     limitsize = F
   )
   
---
+++ R/real_world_analysis/sed/burial_type_210_sed_vs_spatial_distance_create_data.R
@@ -0,0 +1,77 @@
+load("../neomod_datapool/R_data/squared_euclidian_distance_over_time_burial_type.RData")
+load("../neomod_datapool/R_data/distance_matrix_spatial_long.RData")
+
+test <- regions_grid %>%
+  dplyr::mutate(
+    regionA = as.character(regionA),
+    regionB = as.character(regionB)
+  ) 
+
+distance_matrix_spatial_long %<>%
+  dplyr::mutate(
+    regionA = as.character(regionA),
+    regionB = as.character(regionB)
+  ) 
+
+test <- lapply(
+  split(test, f = test$time),
+  function(x) {
+    mn <- pmin(x$regionA, x$regionB)
+    mx <- pmax(x$regionA, x$regionB)
+    int <- as.numeric(interaction(mn, mx))
+    x <- x[match(unique(int), int),]
+    return(x)
+  }
+) %>%
+  do.call(rbind, .)
+
+sed_spatial_distance <- test %>% dplyr::left_join(
+  distance_matrix_spatial_long, by = c("regionA", "regionB")
+) %>% 
+  dplyr::filter(
+    distance != 0
+  ) %>%
+  dplyr::mutate(
+    relation = paste(regionA, "+", regionB),
+    time = base::cut(
+      time, 
+      seq(-2200, -800, 200), labels = paste(seq(-2200, -1000, 200), seq(-2000, -800, 200), sep = " - "),
+      include.lowest = TRUE, 
+      right = FALSE)
+  ) %>%
+  dplyr::group_by(
+    time, regionA, regionB, distance
+  ) %>%
+  dplyr::summarise(
+    mean_sed = mean(sed, na.rm = TRUE)
+  ) %>%
+  dplyr::ungroup() %>%
+  dplyr::filter(
+    !is.na(mean_sed)
+  )
+
+regions_factorA <- as.factor(sed_spatial_distance$regionA)
+sed_spatial_distance$regionA <- factor(regions_factorA, levels = c(
+  "Austria and Czechia",
+  "Poland",
+  "Southern Germany",
+  "Northeast France",
+  "Northern Germany",
+  "Southern Skandinavia",
+  "Benelux",
+  "England"
+))
+
+regions_factorB <- as.factor(sed_spatial_distance$regionB)
+sed_spatial_distance$regionB <- factor(regions_factorB, levels = c(
+  "Austria and Czechia",
+  "Poland",
+  "Southern Germany",
+  "Northeast France",
+  "Northern Germany",
+  "Southern Skandinavia",
+  "Benelux",
+  "England"
+))
+
+save(sed_spatial_distance, file = "../neomod_datapool/R_data/squared_euclidian_distance_over_timeblocks_burial_type.RData")
