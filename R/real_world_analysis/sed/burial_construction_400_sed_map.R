d4887623b015dcb530c64ded9e003fe8099e1622
nevrome <clemens@nevrome.de>
2018-07-08 17:22:05 +0200

    started to work on diachronic sed map

--- R/real_world_analysis/sed/burial_construction_400_sed_map.R
+++ R/real_world_analysis/sed/burial_construction_400_sed_map.R
@@ -1,91 +1,15 @@
-load("../neomod_datapool/bronze_age/regions.RData")
-load("../neomod_datapool/bronze_age/regions_mean_sed")
-
 land_outline <- sf::st_read("../neomod_datapool/geodata/land_shapes/ne_50m_land.shp")
-rivers <- sf::st_read("../neomod_datapool/geodata/rivers_lakes_shapes/ne_50m_rivers_lake_centerlines_scale_rank.shp")
-lakes <- sf::st_read("../neomod_datapool/geodata/rivers_lakes_shapes/ne_50m_lakes.shp")
+countries <- sf::st_read("../neomod_datapool/geodata/country_areas/ne_50m_admin_0_countries.shp")
 research_area <- sf::st_read("manually_changed_data/research_area.shp")
+load("../neomod_datapool/R_data/regions.RData")
+load("../neomod_datapool/R_data/sed_time_spatial_network.RData")
 
-region_centers <- regions %>%
-  sf::st_centroid()
-
-sfc_as_cols <- function(x, names = c("x","y")) {
-  stopifnot(inherits(x,"sf") && inherits(sf::st_geometry(x),"sfc_POINT"))
-  ret <- do.call(rbind,sf::st_geometry(x))
-  ret <- tibble::as_tibble(ret)
-  stopifnot(length(names) == ncol(ret))
-  ret <- setNames(ret,names)
-  dplyr::bind_cols(x,ret)
-}
-
-region_centers %>%
-  sfc_as_cols() %>%
-  dplyr::select(
-    NAME, x, y
-  )
-
-# regions_sed_lines <- regions_grid_mean %>%
-#   dplyr::left_join(
-#     region_centers,
-#     by = c("regionA" = "NAME")
-#   ) %>%
-#   dplyr::left_join(
-#     region_centers,
-#     by = c("regionB" = "NAME"),
-#     suffix = c("_regionA", "_regionB")
-#   ) %>%
-#   dplyr::rowwise() %>%
-#   dplyr::mutate(
-#     geometry = st_cast(
-#       sf::st_combine(c(geometry_regionA, geometry_regionB)),
-#       "LINESTRING"
-#     )
-#   ) %>%
-#   dplyr::ungroup() %>%
-#   dplyr::select(
-#     regionA, regionB, mean_sed, geometry
-#   ) %>%
-#   sf::st_as_sf(crs = st_crs(102013))
-
-mean_sed <- regions_grid_mean %>%
-  dplyr::left_join(
-    region_centers,
-    by = c("regionA" = "NAME")
-  ) %>%
-  dplyr::left_join(
-    region_centers,
-    by = c("regionB" = "NAME"),
-    suffix = c("_regionA", "_regionB")
-  ) %>%
-  dplyr::rowwise() %>%
-  dplyr::mutate(
-    x_a = st_coordinates(geometry_regionA)[,1],
-    y_a = st_coordinates(geometry_regionA)[,2],
-    x_b = st_coordinates(geometry_regionB)[,1],
-    y_b = st_coordinates(geometry_regionB)[,2]
-  ) %>%
-  dplyr::ungroup() %>%
-  dplyr::select(
-    regionA, regionB, mean_sed, x_a, y_a, x_b, y_b
-  ) %>%
-  dplyr::filter(
-    regionA != regionB
-  )
-
-# remove duplicates
-mn <- pmin(mean_sed$regionA, mean_sed$regionB)
-mx <- pmax(mean_sed$regionA, mean_sed$regionB)
-int <- as.numeric(interaction(mn, mx))
-mean_sed <- mean_sed[match(unique(int), int),]
-
+ex <- raster::extent(regions %>% sf::st_transform(sf::st_crs(102013)))
+xlimit <- c(ex[1], ex[2])
+ylimit <- c(ex[3], ex[4])
 
 library(ggplot2)
 library(sf)
-
-ex <- raster::extent(research_area %>% sf::st_transform(sf::st_crs(102013)))
-
-xlimit <- c(ex[1], ex[2])
-ylimit <- c(ex[3], ex[4])
 
 hu <- ggplot() +
   geom_sf(
@@ -93,40 +17,30 @@ hu <- ggplot() +
     fill = "white", colour = "black", size = 0.4
   ) +
   geom_sf(
-    data = rivers,
-    fill = NA, colour = "black", size = 0.2
-  ) +
-  geom_sf(
-    data = lakes,
-    fill = NA, colour = "black", size = 0.2
-  ) +
-  # geom_sf(
-  #   data = regions,
-  #   fill = NA, colour = "red", size = 0.5
-  # ) +
-  geom_sf(
     data = research_area,
     fill = NA, colour = "red", size = 0.5
   ) +
   geom_curve(
-    data = mean_sed,
+    data = distance_lines,
     mapping = aes(
       x = x_a, y = y_a, xend = x_b, yend = y_b,
-      color = regionA, size = mean_sed    
+      size = mean_sed    
     ), 
     alpha = 0.5,
-    curvature = 0.2
+    curvature = 0.2,
+    colour = "black"
   ) +
-  # scale_color_manual(
-  #   #values = c("#999999", "#ffe500", "#56B4E9", "#009E73", "#000000", "#0072B2", "#D55E00", "#CC79A7")
+  facet_wrap(
+    nrow = 2,
+    ~time
+  ) +
+  # geom_sf(
+  #   data = region_centers, 
+  #   mapping = aes(
+  #     colour = NAME
+  #   ), 
+  #   fill = NA, size = 16
   # ) +
-  scale_size_continuous(
-    range = c(5, 0.5)
-  ) +
-  geom_sf(
-    data = region_centers,
-    fill = NA, colour = "red", size = 0.5
-  ) +
   theme_bw() +
   coord_sf(
     xlim = xlimit, ylim = ylimit,
@@ -136,24 +50,62 @@ hu <- ggplot() +
     plot.title = element_text(size = 30, face = "bold"),
     legend.position = "bottom",
     legend.title = element_text(size = 20, face = "bold"),
-    # axis.title = element_blank(),
-    axis.text = element_text(size = 15),
+    axis.title = element_blank(),
+    axis.text = element_blank(),
     legend.text = element_text(size = 20),
-    panel.grid.major = element_line(colour = "black", size = 0.3)
+    panel.grid.major = element_line(colour = "black", size = 0.3),
+    strip.text.x = element_text(size = 20)
   ) +
   guides(
-    color = FALSE,
-    shape = FALSE,
-    size = FALSE
+    size = FALSE,
+    alpha = FALSE
+  ) +
+  scale_color_manual(
+    values = c(
+      "cremation" = "#D55E00",
+      "inhumation" = "#0072B2",
+      "mound" = "#CC79A7",
+      "flat" = "#009E73",
+      "unknown" = "darkgrey",
+      "Austria and Czechia" = "#999999", 
+      "Poland" = "#ffe500", 
+      "Southern Germany" = "#56B4E9", 
+      "Northeast France" = "#009E73", 
+      "Northern Germany" = "#000000", 
+      "Southern Skandinavia" = "#0072B2", 
+      "Benelux" = "#D55E00", 
+      "England" = "#CC79A7"
+    ),
+    breaks = c(
+      "Austria and Czechia",
+      "Poland", 
+      "Southern Germany", 
+      "Northeast France", 
+      "Northern Germany", 
+      "Southern Skandinavia", 
+      "Benelux", 
+      "England"
+    ),
+    labels = c(
+      "Austria and Czechia",
+      "Poland", 
+      "Southern Germany", 
+      "Northeast France", 
+      "Northern Germany", 
+      "Southern Skandinavia", 
+      "Benelux", 
+      "England"
+    )
   )
 
 hu %>%
   ggsave(
-    "/home/clemens/neomod/neomod_datapool/bronze_age/general_map_squared_euclidian_distance.jpeg",
+    "/home/clemens/neomod/neomod_datapool/plots/sed/sed_map_research_area_timeslices.jpeg",
     plot = .,
     device = "jpeg",
     scale = 1,
     dpi = 300,
-    width = 350, height = 360, units = "mm",
+    #width = 210, height = 297, units = "mm",
+    width = 550, height = 280, units = "mm",
     limitsize = F
   )
