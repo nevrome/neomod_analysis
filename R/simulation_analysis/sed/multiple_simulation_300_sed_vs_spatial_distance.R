b9f96287b5fe268fc77aa990bb5d0903ce6d58d6
nevrome <clemens@nevrome.de>
2018-07-08 15:38:59 +0200

    outsourcing of data preparation code

--- R/simulation_analysis/sed/multiple_simulation_300_sed_vs_spatial_distance.R
+++ R/simulation_analysis/sed/multiple_simulation_300_sed_vs_spatial_distance.R
@@ -1,78 +1,14 @@
-load("../neomod_datapool/simulation_data/squared_euclidian_distance_over_time_sim_multiple.RData")
-load("../neomod_datapool/R_data/distance_matrix_spatial_long.RData")
+load("../neomod_datapool/simulation_data/squared_euclidian_distance_over_timeblocks_multiple_simulations.RData")
+sed_simulation <- sed_spatial_distance
 load("../neomod_datapool/R_data/squared_euclidian_distance_over_timeblocks_burial_type.RData")
-sed_burial_type <- sed_spatial_distance
+sed_burial_type <- sed_spatial_distance %>% dplyr::mutate(context = "burial_type")
 load("../neomod_datapool/R_data/squared_euclidian_distance_over_timeblocks_burial_construction.RData")
-sed_burial_construction <- sed_spatial_distance
-
-sed_burial_type %<>%
-  dplyr::mutate(
-    context = "burial_type"
-  )
-
-sed_burial_construction %<>%
-  dplyr::mutate(
-    context = "burial_construction"
-  )
-
-sed_spatial_real_world <- rbind(sed_burial_type, sed_burial_construction)
-
-# test <- regions_grid %>%
-#   dplyr::mutate(
-#     regionA = as.character(regionA),
-#     regionB = as.character(regionB)
-#   ) 
-
-test <- regions_grid
-
-test <- pbapply::pblapply(
-  base::split(test, f = test$model_group), function(z) { 
-    lapply(
-      base::split(z, f = test$model_id), function(y) { 
-        lapply(
-          base::split(y, f = y$time), function(x) {
-            mn <- pmin(x$regionA, x$regionB)
-            mx <- pmax(x$regionA, x$regionB)
-            int <- as.numeric(interaction(mn, mx))
-            x <- x[match(unique(int), int),]
-            return(x)
-          }) %>%
-        do.call(rbind, .)
-    }) %>%
-    do.call(rbind, .)    
-}) %>%
-  do.call(rbind, .)
-
-
-hu <- test %>% dplyr::left_join(
-    distance_matrix_spatial_long, by = c("regionA", "regionB")
-  ) %>% 
-  dplyr::filter(
-    distance != 0
-  ) %>%
-  dplyr::mutate(
-    relation = paste(regionA, "+", regionB),
-    time = base::cut(
-      time, 
-      seq(-2200, -800, 200), labels = paste(seq(-2200, -1000, 200), seq(-2000, -800, 200), sep = " - "),
-      include.lowest = TRUE, 
-      right = FALSE)
-  ) %>%
-  dplyr::group_by(
-    model_id, model_group, time, regionA, regionB, distance
-  ) %>%
-  dplyr::summarise(
-    mean_sed = mean(sed, na.rm = TRUE)
-  ) %>%
-  dplyr::ungroup() %>%
-  dplyr::filter(
-    !is.na(mean_sed)
-  )
+sed_burial_construction <-sed_spatial_distance %>% dplyr::mutate(context = "burial_construction")
 
 library(ggplot2)
 plu <- ggplot() +
   geom_boxplot(
-    data = hu,
+    data = sed_simulation,
     mapping = aes(
       x = as.factor(distance), 
       y = mean_sed
