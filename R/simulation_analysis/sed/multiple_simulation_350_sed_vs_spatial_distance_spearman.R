7c795fcf8ae21376add85dba3f0a56e213e20f34
nevrome <clemens@nevrome.de>
2018-07-09 14:22:44 +0200

    added connecting lines to plot to make it complete

--- R/simulation_analysis/sed/multiple_simulation_350_sed_vs_spatial_distance_spearman.R
+++ R/simulation_analysis/sed/multiple_simulation_350_sed_vs_spatial_distance_spearman.R
@@ -24,30 +24,25 @@ ju <- ggplot() +
     colour = "red",
     size = 2
   ) + 
-  geom_boxplot(
+  geom_line(
+    data = mantel_simulations,
+    mapping = aes(
+      x = time,
+      y = statistic,
+      group = model_id
+    ),
+    position = position_nudge(x = -0.25),
+    size = 0.2,
+    alpha = 0.2
+  ) +
+  geom_point(
     data = mantel_simulations,
     mapping = aes(
       x = time,
       y = statistic
     ),
-    width = 0.2
-  ) +
-  geom_dotplot(
-    data = mantel_simulations,
-    mapping = aes(
-      x = time,
-      y = statistic,
-      fill = base::cut(
-        signif, 
-        breaks = c(0, 0.01, 0.05, 0.1, seq(0.2, 1, 0.1)), 
-        labels = c("< 0.01", "< 0.05", "< 0.1", rep("> 0.1", 9))
-      )
-    ),
-    binaxis = "y",
-    stackdir = "down",
-    position = position_nudge(x = -0.4),
-    dotsize = 0.35,
-    binpositions = "all"
+    position = position_nudge(x = -0.25),
+    size = 1
   ) +
   geom_point(
     data = mantel_real_world,
@@ -68,7 +63,32 @@ ju <- ggplot() +
       group = context
     ),
     position = position_nudge(x = -0.25),
-    size = 0.5
+    size = 0.7
+  ) +
+  geom_boxplot(
+    data = mantel_simulations,
+    mapping = aes(
+      x = time,
+      y = statistic
+    ),
+    width = 0.15
+  ) +
+  geom_dotplot(
+    data = mantel_simulations,
+    mapping = aes(
+      x = time,
+      y = statistic,
+      fill = base::cut(
+        signif, 
+        breaks = c(0, 0.01, 0.05, 0.1, seq(0.2, 1, 0.1)), 
+        labels = c("< 0.01", "< 0.05", "< 0.1", rep("> 0.1", 9))
+      )
+    ),
+    binaxis = "y",
+    stackdir = "down",
+    position = position_nudge(x = -0.4),
+    dotsize = 0.35,
+    binpositions = "all"
   ) +
   scale_fill_manual(
     name = "Mantel test significance level of simulation runs",
