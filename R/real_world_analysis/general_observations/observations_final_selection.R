795949e095f6f472e7328b614addf83e2846e053
nevrome <clemens@nevrome.de>
2018-08-07 17:33:47 +0200

    some more basic observations

--- R/real_world_analysis/general_observations/observations_final_selection.R
+++ R/real_world_analysis/general_observations/observations_final_selection.R
@@ -4,6 +4,11 @@ dpr <- dates_per_region
 
 # number of sites
 dpr$site %>% unique %>% length()
+dpr$period %>% unique %>% length()
+dpr$culture %>% unique %>% length()
 
-# distribution of materiels
+# distributions
 dpr$material %>% table(useNA = "always")
+dpr$species %>% table(useNA = "always")
+dpr$burial_type %>% table()
+dpr$burial_construction %>% table()
