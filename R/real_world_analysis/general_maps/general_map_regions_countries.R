de9750fa384c056bad0c10df19c5a622eb47f5b9
nevrome <clemens@nevrome.de>
2018-08-10 17:32:06 +0200

    new map: regions and country borders

---
+++ R/real_world_analysis/general_maps/general_map_regions_countries.R
@@ -0,0 +1,143 @@
+#load("../neomod_datapool/bronze_age/space_and_network/land_outline_sf.RData")
+load("data_analysis/bronze1.RData")
+land_outline <- sf::st_read("../neomod_datapool/geodata/land_shapes/ne_50m_land.shp")
+countries <- rnaturalearth::ne_download(scale = 50, type = 'countries', category = 'cultural') %>% sf::st_as_sf()
+rivers <- sf::st_read("../neomod_datapool/geodata/rivers_lakes_shapes/ne_50m_rivers_lake_centerlines_scale_rank.shp")
+lakes <- sf::st_read("../neomod_datapool/geodata/rivers_lakes_shapes/ne_50m_lakes.shp")
+research_area <- sf::st_read("data_manually_prepared/research_area.shp")
+load("data_analysis/regions.RData")
+
+regions_name_points <- regions %>%
+  dplyr::mutate(
+    x = purrr::map_dbl(geometry, ~sf::st_centroid(.x)[[1]]),
+    y = purrr::map_dbl(geometry, ~sf::st_centroid(.x)[[2]])
+  )
+
+bronze1_sf <- bronze1 %>% sf::st_as_sf(
+  coords = c("lon", "lat"),
+  crs = 4326
+)
+
+library(ggplot2)
+library(sf)
+
+ex <- raster::extent(research_area %>% sf::st_transform(sf::st_crs(102013)))
+
+xlimit <- c(ex[1], ex[2])
+ylimit <- c(ex[3], ex[4])
+
+hu <- ggplot() +
+  geom_sf(
+    data = land_outline,
+    fill = "white", colour = "black", size = 0.4
+  ) +
+  geom_sf(
+    data = countries,
+    fill = NA, colour = "black", size = 0.2
+  ) +
+  geom_sf(
+    data = regions,
+    mapping = aes(
+      colour = NAME
+    ),
+    fill = NA, size = 3.5
+  ) +
+  geom_sf(
+    data = research_area,
+    fill = NA, colour = "red", size = 0.5
+  ) +
+  geom_sf(
+    data = bronze1_sf,
+    mapping = aes(
+      color = burial_type,
+      shape = burial_construction,
+      size = burial_construction
+    ),
+    show.legend = "point"
+  ) +
+  # geom_text(
+  #   data = regions_name_points,
+  #   aes(x = x, y = y, label = ID),
+  #   size = 15
+  # ) +
+  theme_bw() +
+  coord_sf(
+    xlim = xlimit, ylim = ylimit,
+    crs = st_crs(102013)
+  ) + 
+  scale_shape_manual(
+    values = c(
+      "flat" = "\u268A",
+      "mound" = "\u25E0",
+      "unknown" = "\u2715"
+    )
+  ) +
+  scale_size_manual(
+    values = c(
+      "flat" = 10,
+      "mound" = 10,
+      "unknown" = 5
+    )
+  ) +
+  scale_color_manual(
+    values = c(
+      "cremation" = "#D55E00",
+      "inhumation" = "#0072B2",
+      "mound" = "#CC79A7",
+      "flat" = "#009E73",
+      "unknown" = "darkgrey",
+      "Austria and Czechia" = "#999999", 
+      "Poland" = "#ffe500", 
+      "Southern Germany" = "#56B4E9", 
+      "Northeastern France" = "#009E73", 
+      "Northern Germany" = "#000000", 
+      "Southern Skandinavia" = "#0072B2", 
+      "Benelux" = "#D55E00", 
+      "England" = "#CC79A7"
+    ),
+    breaks = c(
+      "Austria and Czechia",
+      "Poland", 
+      "Southern Germany", 
+      "Northeastern France", 
+      "Northern Germany", 
+      "Southern Skandinavia", 
+      "Benelux", 
+      "England"
+    ),
+    labels = c(
+      "Austria and Czechia",
+      "Poland", 
+      "Southern Germany", 
+      "Northeastern France", 
+      "Northern Germany", 
+      "Southern Skandinavia", 
+      "Benelux", 
+      "England"
+    )
+  ) +
+  theme(
+    plot.title = element_text(size = 30, face = "bold"),
+    legend.position = "bottom",
+    legend.title = element_text(size = 20, face = "bold"),
+    axis.title = element_blank(),
+    axis.text = element_text(size = 15),
+    legend.text = element_text(size = 20),
+    panel.grid.major = element_line(colour = "black", size = 0.3)
+  ) +
+  guides(
+    color = guide_legend(title = NULL, override.aes = list(size = 10), nrow = 2, byrow = TRUE),
+    shape = FALSE,
+    size = FALSE
+  )
+
+hu %>%
+  ggsave(
+    "figures_plots/general_maps/general_map_regions_countries.jpeg",
+    plot = .,
+    device = "jpeg",
+    scale = 1,
+    dpi = 300,
+    width = 350, height = 320, units = "mm",
+    limitsize = F
+  )
