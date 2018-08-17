82b069176ff6ce9160b75b2399ac358f160ccd85
nevrome <clemens@nevrome.de>
2018-08-17 18:08:25 +0200

    New attempt at the chronology plot

--- R/other_analysis/bronze_age_chronology_gantt.R
+++ R/other_analysis/bronze_age_chronology_gantt.R
@@ -1,6 +1,5 @@
-chronology <- readr::read_delim(
-  "data_manually_prepared/bronze_age_chronology.csv",
-  delim = "\t"
+chronology <- readr::read_csv(
+  "data_manually_prepared/bronze_age_chronology.csv"
   )
 
 chronology %<>% dplyr::mutate(
@@ -8,69 +7,82 @@ chronology %<>% dplyr::mutate(
   end_date = replace(end_date, is.na(end_date), Inf), 
   label_pos = (start_date + end_date) / 2, 
   unit_name = factor(as.factor(unit_name), levels = unique(as.factor(unit_name)[order(start_date)])),
-  spatial_context = factor(as.factor(spatial_context), levels = rev(c(
-   "Britain", "France", "Scandinavia", "Central Europe", "North and central Italy", "Spain"
-  )))
+  spatial_context = as.factor(spatial_context)
+  # spatial_context = factor(as.factor(spatial_context), levels = rev(c(
+  #  "Britain", "France", "Scandinavia", "Central Europe", "North and central Italy", "Spain"
+  # )))
   )
 
-dodge_value <- 1.4
-line_width <- 6
-  
+separators <- chronology %>%
+  dplyr::select(-reference, -start_date_pre, -end_date_post, -unit_name, -label_pos) %>%
+  tidyr::gather(
+    unit, dates, -spatial_context
+  )
+
+line_width = 20
+
 library(ggplot2)
-hu <- chronology %>%
   ggplot() +
   geom_linerange(
-    aes(
+    data = chronology, 
+    mapping = aes(
       x = spatial_context,
       ymin = start_date_pre,
       ymax = start_date,
       group = unit_name
     ),
     size = line_width,
-    color = "darkgrey",
-    position = position_dodge(dodge_value)
+    color = "darkgrey"
   ) +
   geom_linerange(
-    aes(
+    data = chronology, 
+    mapping = aes(
       x = spatial_context,
       ymin = end_date,
       ymax = end_date_post,
       group = unit_name
     ),
     size = line_width,
-    color = "darkgrey",
-    position = position_dodge(dodge_value)
+    color = "darkgrey"
   ) +
   geom_linerange(
-    aes(
+    data = chronology, 
+    mapping = aes(
       x = spatial_context,
       ymin = start_date,
       ymax = end_date,
       group = unit_name,
       color = spatial_context
     ),
-    size = line_width,
-    position = position_dodge(dodge_value)
+    size = line_width
+  ) +
+  geom_segment(
+    data = separators,
+    mapping = aes(
+      x = as.numeric(spatial_context) - 0.2,
+      xend = as.numeric(spatial_context) + 0.2,
+      y = dates,
+      yend = dates
+    )
   ) +
   geom_text(
-    aes(
+    data = chronology, 
+    mapping = aes(
       x = spatial_context,
       y = label_pos,
       label = unit_name,
       group = unit_name
     ),
-    position = position_dodge(dodge_value),
     size = 3
   ) +
-  coord_flip(ylim = c(-2550, -500)) + 
   theme_bw() +
   guides(color = FALSE) +
   xlab("") +
   ylab("Time in years calBC") +
-  scale_y_continuous(
-    breaks = seq(-2500, -500, 200),
-    limits = c(-2550, -500)
-  )
+  scale_y_reverse(
+    breaks = seq(-800, -2200, -200),
+    limits = c(-800, -2200)
+  ) 
 
 # hu %>%
 #   ggsave(
