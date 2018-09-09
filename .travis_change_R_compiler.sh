b1965ee31930fee703da46cce05e5ca3940ebce6
nevrome <clemens@nevrome.de>
2018-09-09 22:07:37 +0200

    better compiler options?

--- .travis_change_R_compiler.sh
+++ .travis_change_R_compiler.sh
@@ -2,6 +2,6 @@
 
 mkdir ~/.R
 touch ~/.R/Makevars
-echo "CC=gcc-4.8" >> ~/.R/Makevars
+echo "CC=gcc-4.8 -std=gnu99" >> ~/.R/Makevars
 echo "CXX=g++-4.8 -std=gnu++11" >> ~/.R/Makevars
 echo "PKG_CXXFLAGS= -stdlib=libc++ -std=c++11" >> ~/.R/Makevars
