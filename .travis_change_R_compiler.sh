47b4297d566597500849e9516bd97733985b02bf
nevrome <clemens@nevrome.de>
2018-09-09 23:32:36 +0200

    travis: wrong compiler option

--- .travis_change_R_compiler.sh
+++ .travis_change_R_compiler.sh
@@ -4,4 +4,4 @@ mkdir ~/.R
 touch ~/.R/Makevars
 echo "CC=gcc-4.8 -std=gnu99" >> ~/.R/Makevars
 echo "CXX=g++-4.8 -std=gnu++11" >> ~/.R/Makevars
-echo "PKG_CXXFLAGS= -stdlib=libc++ -std=c++11" >> ~/.R/Makevars
+echo "PKG_CXXFLAGS= -std=c++11" >> ~/.R/Makevars
