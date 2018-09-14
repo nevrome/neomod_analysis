fa60c5c467a471210312163314d7f4178d72511e
nevrome <clemens@nevrome.de>
2018-09-14 23:02:38 +0200

    improvements of distance comparison plot

--- R/simulation/sed/500_multiple_simulation_sed_vs_spatial_distance.R
+++ R/simulation/sed/500_multiple_simulation_sed_vs_spatial_distance.R
@@ -1,39 +1,24 @@
 load("data_simulation/sed_simulation_regions_timeslices_spatial_distance.RData")
 sed_simulation <- sed_spatial_distance
 load("data_analysis/squared_euclidian_distance_over_timeblocks_burial_type.RData")
-sed_burial_type <- sed_spatial_distance %>% dplyr::mutate(context = "burial_type")
+sed_burial_type <- sed_spatial_distance %>% dplyr::mutate(model_group = "burial_type")
 load("data_analysis/squared_euclidian_distance_over_timeblocks_burial_construction.RData")
-sed_burial_construction <- sed_spatial_distance %>% dplyr::mutate(context = "burial_construction")
+sed_burial_construction <- sed_spatial_distance %>% dplyr::mutate(model_group = "burial_construction")
+
+sed_all <- sed_simulation %>%
+  dplyr::select(-model_id) %>%
+  rbind(sed_burial_type) %>%
+  rbind(sed_burial_construction)
 
 library(ggplot2)
-plu <- ggplot() +
+plu <- ggplot(sed_all) +
   geom_boxplot(
-    data = sed_simulation,
     mapping = aes(
-      x = as.factor(distance), 
-      y = mean_sed
-    ),
-    width = 0.3
-  ) +
-  geom_point(
-    data = sed_burial_type,
-    mapping  = aes(
-      x = as.factor(distance), 
+      x = as.factor(distance),
       y = mean_sed,
-      colour = context
+      fill = model_group
     ),
-    size = 4,
-    position = position_nudge(x = -0.3)
-  ) +
-  geom_point(
-    data = sed_burial_construction,
-    mapping  = aes(
-      x = as.factor(distance), 
-      y = mean_sed,
-      colour = context
-    ),
-    size = 4,
-    position = position_nudge(x = -0.5)
+    width = 0.8
   ) +
   facet_wrap(
     nrow = 2,
@@ -43,7 +28,9 @@ plu <- ggplot() +
     name = "Real world context",
     values = c(
       "burial_type" = "#0072B2",
-      "burial_construction" = "#009E73"
+      "burial_construction" = "#009E73",
+      "1" = "#0072B2",
+      "2" = "#009E73"
     )
   ) +
   theme_bw() +
@@ -58,6 +45,7 @@ plu <- ggplot() +
   ) +
   xlab("Spatial Distance Classes") +
   ylab("Squared Euclidian Distance") +
+  ylim(0, 2) +
   NULL
 
 plu %>%
