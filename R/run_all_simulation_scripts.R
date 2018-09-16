bff39789ceea98a7897f987ee1a906fd742eb545
nevrome <clemens@nevrome.de>
2018-09-16 20:45:51 +0200

    new simulation run script

--- R/run_all_simulation_scripts.R
+++ R/run_all_simulation_scripts.R
@@ -8,7 +8,9 @@ lapply(
   c(
     "R/simulation/parameter_exploration/popsize_crossregions",
     "R/simulation/parameter_exploration/vertitrans",
-    "R/simulation/parameter_exploration/startprop_distancemat"
+    "R/simulation/parameter_exploration/startprop_distancemat",
+    "R/simulation/sed",
+    "R/simulation/population_graph_visualization/"
   ), 
   function(x) {
     pbapply::pblapply(
